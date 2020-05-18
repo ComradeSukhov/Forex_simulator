2ea792ab9bcb429d5a70d1350b0f41bfb2b1d0e4
Dmitry <dmitry-samoilov.l@yandex.ru>
2020-05-18 11:40:31 +0300

    Первое приближение к реализации вертикальной шкалы на графике

--- J_candles.rb
+++ J_candles.rb
@@ -4,7 +4,7 @@ require 'rmagick'
 require 'json'
 require_relative 'methods.rb'
 
-rate = JSON.parse(File.read('candles_db.json'))
+rate = JSON.parse(File.read('candles_db.json')).transform_keys{ |key| key.to_i}
 
 density          = 14 # плотность отображения японских свечей
 thickness        = 10 # толщина одной свечи
@@ -12,7 +12,7 @@ vertical_padding = 10 # отступ он верха и низа
 
 top_extremum = to_points(rate.map { |x| x[1]['max']}.max)
 low_extremum = to_points(rate.map { |x| x[1]['min']}.min)
-
+amplitude    = top_extremum - low_extremum
 
 scale_ratio = (720.0 - vertical_padding * 2) / (top_extremum - low_extremum)
 
@@ -20,52 +20,86 @@ scale_ratio = (720.0 - vertical_padding * 2) / (top_extremum - low_extremum)
 canvas = Magick::ImageList.new
 canvas.new_image(1280, 720, Magick::HatchFill.new('white', 'gray93'))
 
-candle = Magick::Draw.new
-candle.stroke('green')
-candle.fill('green')
-candle.stroke_width(1)
+candles = Magick::Draw.new
+candles.stroke('green')
+candles.fill('green')
+candles.stroke_width(1)
 
 50.times do |i|
-  if rate[i.to_s]['start'] < rate[i.to_s]['finish']
-    candle.fill_opacity(1)
+  if rate[i]['start'] < rate[i]['finish']
+    candles.fill_opacity(1)
   else
-    candle.fill_opacity(0)
+    candles.fill_opacity(0)
   end
 
-  candle.rectangle(
+  candles.rectangle(
     i * density,
-    (top_extremum - to_points(rate[i.to_s]["start"])) * scale_ratio + 10,
+    (top_extremum - to_points(rate[i]["start"])) * scale_ratio + 10,
 
     i * density + thickness,
-    (top_extremum - to_points(rate[i.to_s]["finish"])) * scale_ratio + 10 + 1
+    (top_extremum - to_points(rate[i]["finish"])) * scale_ratio + 10 + 1
   )
 
 
 
-  high_end      = [rate[i.to_s]['start'], rate[i.to_s]['finish']].max
-  low_end       = [rate[i.to_s]['start'], rate[i.to_s]['finish']].min
-  candle_center = i * density + thickness / 2
+  high_end      = [rate[i]['start'], rate[i]['finish']].max
+  low_end       = [rate[i]['start'], rate[i]['finish']].min
+  candles_center = i * density + thickness / 2
 
-  if rate[i.to_s]['max'] != high_end
-    candle.line(
-      candle_center,
+  if rate[i]['max'] != high_end
+    candles.line(
+      candles_center,
       (top_extremum - to_points(high_end)) * scale_ratio + 10,
 
-      candle_center,
-      (top_extremum - to_points(rate[i.to_s]["max"])) * scale_ratio + 10      
+      candles_center,
+      (top_extremum - to_points(rate[i]["max"])) * scale_ratio + 10      
     )
   end
 
-  if rate[i.to_s]['min'] != low_end
-  candle.line(
-      candle_center,
+  if rate[i]['min'] != low_end
+  candles.line(
+      candles_center,
       (top_extremum - to_points(low_end)) * scale_ratio + 10 + 1,
 
-      candle_center,
-      (top_extremum - to_points(rate[i.to_s]["min"])) * scale_ratio + 10      
+      candles_center,
+      (top_extremum - to_points(rate[i]["min"])) * scale_ratio + 10      
     )
   end
 end
 
-candle.draw(canvas)
+
+
+
+page_bottom = (low_extremum - 10 / scale_ratio).ceil
+page_top    = (top_extremum + 10 / scale_ratio).floor
+step        = scale_step(amplitude)
+
+
+first_mark = (page_bottom..).find{ |x| x % step == 0 }
+
+
+
+left_scale = Magick::Draw.new
+
+left_scale.stroke('black')
+left_scale.stroke_width(1)
+left_scale.pointsize(12)
+left_scale.line(10,0, 10,720)
+
+first_mark.step(page_top, step) do |mark|
+
+left_scale.stroke_width(1)
+  left_scale.line(10,(top_extremum - mark) * scale_ratio + 10,
+                  20,(top_extremum - mark) * scale_ratio + 10)
+
+left_scale.text_undercolor('white')
+left_scale.stroke_width(0)
+
+left_scale.text(25,(top_extremum - mark) * scale_ratio + 10 + 4,
+                  mark.to_s.insert(1, "."))
+
+end
+
+candles.draw(canvas)
+left_scale.draw(canvas)
 canvas.write('test.jpg')
--- methods.rb
+++ methods.rb
@@ -1,3 +1,54 @@
 def to_points(rate_value)
   (rate_value * 10_000).round  
 end
+
+
+
+def scale_step(amplitude)
+  
+  case amplitude
+  when 0..5
+    1
+  when 6..12
+    2
+  when 13..22
+    5
+  when 23..45
+    10
+  when 46..90
+    20
+  when 91..110
+    25
+  when 111..180
+    40
+  when 181..270
+    50
+  when 271..320
+    75
+  when 321...650
+    100  
+  else
+    
+  handsome_round(amplitude)
+
+  end
+end
+
+def handsome_round(amplitude)
+  number = amplitude / 5    
+
+  array = number.digits.reverse
+
+  if (3..7).any?(array[1])
+    array[1] = 5
+  elsif (0..2).any?(array[1])
+    array[1] = 0
+  else
+    array[1] = 0
+    array[0] += 1
+  end
+
+  (2...array.size).each{ |i| array[i] = 0 }
+
+  array.join.to_i
+end
